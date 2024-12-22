70649d296fdf90ba348cda5da0c1b0eb6057cbef
alvinhowse <ahowse@tutamail.com>
2024-12-22 15:09:18 +0000

    Source files

---
+++ oi.js
@@ -0,0 +1,49 @@
+let elements = document.getElementsByClassName("oa-folder");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "A";
+}
+
+elements = document.getElementsByClassName("oa-file");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "B";
+}
+
+elements = document.getElementsByClassName("oa-circled-plus");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "C";
+}
+
+elements = document.getElementsByClassName("oa-plus");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "D";
+}
+
+elements = document.getElementsByClassName("oa-circled-cross");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "E";
+}
+
+elements = document.getElementsByClassName("oa-cross");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "F";
+}
+
+elements = document.getElementsByClassName("oa-circled-info");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "G";
+}
+
+elements = document.getElementsByClassName("oa-info");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "H";
+}
+
+elements = document.getElementsByClassName("oa-trash");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "I";
+}
+
+elements = document.getElementsByClassName("oa-external-link");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = "J";
+}
